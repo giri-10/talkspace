22f2d05c8a9addf50fc802bff61530deac8a9702
giri-10 <90170719+giri-10@users.noreply.github.com>
2024-05-03 06:58:22 +0530

    feat: added chat controller

--- src/main/java/com/java/talkspace/config/WebSocketConfig.java
+++ src/main/java/com/java/talkspace/config/WebSocketConfig.java
@@ -1,4 +1,4 @@
-package com.java.chatapp.config;
+package com.java.talkspace.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
--- src/test/java/com/java/talkspace/ChatAppApplicationTests.java
+++ src/test/java/com/java/talkspace/ChatAppApplicationTests.java
@@ -1,4 +1,4 @@
-package com.java.chatapp;
+package com.java.talkspace;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
